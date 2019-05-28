3e3b86d9a902df23e0168850c931e51dfd7a6ee4
Alex Merecka <merecka@gmail.com>
2019-05-28 18:00:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,7 +64,9 @@ class Dog
   end
 
   def self.find_by_name(name)
-
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
   def update
