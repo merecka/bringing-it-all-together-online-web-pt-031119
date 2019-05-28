45b57938d386d63fd2dd441e73be58f10709932f
Alex Merecka <merecka@gmail.com>
2019-05-28 03:26:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,22 @@ class Dog
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
   end
 
+  def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO songs (name, album)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.album)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
+  end
+
+  def update
+
+  end
+
 end
